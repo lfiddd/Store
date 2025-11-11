1e5ed745af4b93c24e1bf3fd0abac734f518347f
Ikhsanovv <ikhsanovbulitt@icloud.com>
2025-11-11 10:21:07 +0300

    mini update

--- Store/Controllers/AdminController.cs
+++ Store/Controllers/AdminController.cs
@@ -13,17 +13,25 @@ public class AdminController
     
     [HttpGet("GetAllEmployee")]
     [RoleAtribute([1])]
-    public async Task<IActionResult> GetAllUsers() => await _userService.GetAllUsers(2);
+    public async Task<IActionResult> GetAllEmployee() => await _userService.GetAllUsers(2);
+    
+    [HttpGet("GetAllUsers")]
+    [RoleAtribute([1])]
+    public async Task<IActionResult> GetAllUsers() => await _userService.GetAllUsers(3);
     
     [HttpPost("CreateNewEmployee")]
     [RoleAtribute([1])]
-    public async Task<IActionResult> CreateUser(UserQuery newUser, int id_role) => await _userService.CreateNewUserAndLogin(newUser, 2);
+    public async Task<IActionResult> CreateEmployee(UserQuery newUser, int id_role) => await _userService.CreateNewUserAndLogin(newUser, 2);
     
-    [HttpPut("{id}/UpdateEmployee")]
+    [HttpPost("CreateNewUser")]
     [RoleAtribute([1])]
-    public async Task<IActionResult> UpdateUser(int id, UserQuery updatedUser) => await _userService.UpdateUserAndLogin(id, updatedUser);
+    public async Task<IActionResult> CreateUser(UserQuery newUser, int id_role) => await _userService.CreateNewUserAndLogin(newUser, 3);
+    
+    [HttpPut("UpdateEmployeeOrUser/{id}")]
+    [RoleAtribute([1])]
+    public async Task<IActionResult> UpdateUserOrEmployee(int id, UserQuery updatedUser) => await _userService.UpdateUserAndLogin(id, updatedUser);
 
-    [HttpDelete("{id}/DeleteEmployee")]
+    [HttpDelete("DeleteUserOrEmployee/{id}")]
     [RoleAtribute([1])]
-    public async Task<IActionResult> DeleteUser(int id) => await _userService.DeleteUser(id);
+    public async Task<IActionResult> DeleteUserOrEmployee(int id) => await _userService.DeleteUser(id);
 }
--- Store/Controllers/EmployeeController.cs
+++ Store/Controllers/EmployeeController.cs
@@ -11,20 +11,20 @@ public class EmployeeController : ControllerBase
     private readonly IUserService _userService;
     public EmployeeController(IUserService userService) => _userService = userService;
 
-    [HttpGet("GetAllUsers")]
-    [RoleAtribute([1, 2])]
+    [HttpGet("GetUsers")]
+    [RoleAtribute([2])]
     public async Task<IActionResult> GetAllUsers() => await _userService.GetAllUsers(3);
     
-    [HttpPost("CreateNewUser")]
-    [RoleAtribute([1, 2])]
+    [HttpPost("CreateUser")]
+    [RoleAtribute([2])]
     public async Task<IActionResult> CreateUser(UserQuery newUser, int id_role) => await _userService.CreateNewUserAndLogin(newUser, 3);
     
     [HttpPut("{id}/UpdateUser")]
-    [RoleAtribute([1, 2])]
+    [RoleAtribute([2])]
     public async Task<IActionResult> UpdateUser(int id, UserQuery updatedUser) => await _userService.UpdateUserAndLogin(id, updatedUser);
 
     [HttpDelete("{id}/DeleteUser")]
-    [RoleAtribute([1, 2])]
+    [RoleAtribute([2])]
     public async Task<IActionResult> DeleteUser(int id) => await _userService.DeleteUser(id);
     
 }
